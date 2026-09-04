ba65fb7748c23f2a598a395f987e6b257bb2cbed
dinakargowdan585 <dinakargowdan585@gmail.com>
2026-09-04 17:36:33 +0530

    fix(standby): fix fullscreen clock vertical centering and cosmic canvas background positioning

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Productive OS Offline Caching & PWA Service Worker */
 
-const CACHE_NAME = "productive-os-cache-v21";
+const CACHE_NAME = "productive-os-cache-v22";
 const ASSETS = [
   "./",
   "./index.html",
