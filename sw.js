e9ef99ea4278f29408bad596cd2f36394cdfca86
dinakargowdan585 <dinakargowdan585@gmail.com>
2026-09-05 11:35:14 +0530

    Update app logo and favicon assets for DHARIN

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* DHARIN Offline Caching & PWA Service Worker */
 
-const CACHE_NAME = "dharin-os-cache-v46";
+const CACHE_NAME = "dharin-os-cache-v47";
 const ASSETS = [
   "./",
   "./index.html",
