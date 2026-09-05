f25f197168372ba6db58a8be7f40bf454c0fe60b
dinakargowdan585 <dinakargowdan585@gmail.com>
2026-09-05 08:55:15 +0530

    Style all unselected input textboxes with dark tinted glass

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Productive OS Offline Caching & PWA Service Worker */
 
-const CACHE_NAME = "productive-os-cache-v35";
+const CACHE_NAME = "productive-os-cache-v36";
 const ASSETS = [
   "./",
   "./index.html",
