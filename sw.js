7c44a65742a531f66f98dcb315002b013de40618
dinakargowdan585 <dinakargowdan585@gmail.com>
2026-09-07 20:22:59 +0530

    Refactor UI strictly adhering to Apple Studio Obsidian & 44pt Ergonomics spec

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* DHARIN Offline Caching & PWA Service Worker */
 
-const CACHE_NAME = "dharin-os-cache-v51";
+const CACHE_NAME = "dharin-os-cache-v52";
 const ASSETS = [
   "./",
   "./index.html",
